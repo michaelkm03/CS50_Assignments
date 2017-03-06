4eb1c82d7bae55890b94eba7fd8ee4bec0b23d0a
michaelkm03 <michael.m@getvictorious.com>
2017-03-06 13:36:25 -0800

    edits to Array Shift

--- Insurance/Insurance/main.c
+++ Insurance/Insurance/main.c
@@ -15,33 +15,40 @@
 float base = 1000.00;
 int age = 0;
 float total;
+int count;
+char n;
 
-//int DeterminePrice(int age, int base) {
-//    if (age >= 16 && age <=19 ){
-//        total = base + (base * 0.15); printf("Insurance Price:  $%.2f%\n", total);}
-//    else if (age >= 20 && age <=25 ){
-//        total = base + (base * 0.05); printf("Insurance Price:  $%.2f%\n", total);}
-//    else if (age > 25 ){
-//        total = base + (base * 0.10); printf("Insurance Price:  $%.2f%\n", total);}
-//    else
-//
-//    return (0);
-//}
+int DeterminePrice(int age, int base) {
+    if (age >= 16 && age <=19 ){
+        total = base + (base * 0.15); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);}
+    else if (age >= 20 && age <=25 ){
+        total = base + (base * 0.05); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);}
+    else {
+        total = base + (base * 0.10); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);
+    }
+    return (0);
+}
 
 int main() {
-    printf("How old are you?  ");
-    scanf("%d", &age);
+    printf("How many in your family?  "); scanf("%d%", &count);
+    int members[count];
+    
+    if (count <= 0 || count > 10)
+        printf("Not Valid.");
+    else
+        for(int i = 0; i < count; i++) {
+            printf("Enter Name of %d%  ", i); scanf("%c%", &n);
+            members[i] = &n;
+        }
+    //printf("How old are you?  "); scanf("%d", &age);
     if (age < 16 )
         return (printf("Out of Range...Too Young\n"));
-    else
+    else{
         printf("[===================== CUSTOMER INFORMATION =====================]\n");
-        if (age >= 16 && age <=19 ){
-            total = base + (base * 0.15); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);}
-        else if (age >= 20 && age <=25 ){
-            total = base + (base * 0.05); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);}
-        else {
-            total = base + (base * 0.10); printf("Age:  %d%\nInsurance Price:  $%.2f%\nSex:  Who knows...\n",age, total);}
-    return 0;
+        DeterminePrice(age, base);
+        printf("[================================================================]\n");
+        
+    return 0;}
 }
 
 
