ce9a27f71c69dff8b6f4afcd1aa87672a03f09c6
michaelkm03 <michael.m@getvictorious.com>
2017-04-18 15:47:41 -0700

    initial commit

---
+++ Two Array Count Letters/Two Array Count Letters/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  Two Array Count Letters
+//
+//  Created by Michael K Montgomery on 4/18/17.
+//  Copyright © 2017 Michael Montgomery. All rights reserved.
+//
+
+/*
+ 
+Write a complete program using two arrays, upper and lower.   Keep the upper and lower alphabet respectively.
+ 
+Ask the user to enter string example:
+This is a test from  Jupiter. Soon you will see who is from Jupiter!!! May be Dr. D.
+ 
+Your program should parse the string and keep track of number of alphabet. Both arrays are indexed from 0 to 25. The
+logical way to do this is to use upper[0] to count the number of ‘A’, and upper[1] to count number of ‘B’ and so on.
+ 
+Output should look like:
+A: 0        a:2
+B: 0 	    b:1
+….
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    
+    
+    return 0;
+}
--- UpperAndLowerCase/UpperAndLowerCase/main.c
+++ UpperAndLowerCase/UpperAndLowerCase/main.c
@@ -13,35 +13,56 @@
 /*
  
  Write a complete program for the following:
-- Ask user to enter sentence
-- Convert the sentence to all capital letters and print
-- Convert the sentence to all lower case letters and print
-- Convert the each character if it is upper to lower and vice versa.
-*/
+ - Ask user to enter sentence
+ - Convert the sentence to all capital letters and print
+ - Convert the sentence to all lower case letters and print
+ - Convert the each character if it is upper to lower and vice versa.
+ */
 
 char string[80];
 int i = 0;
 
-void Upper(char string[80]){
-    while(string[i])
+// Function that, while index exists in string, puts char to uppercase
+void Upper(int i, char string[80]){
+    while(string[i]!='\0')
     {
         putchar (toupper(string[i]));
         i++;
     }
 }
-void Lower(char string[80]){
-    while(string[i])
+
+// Function that, while index exists in string, puts char to lowercase
+void Lower(int i, char string[80]){
+    while(string[i]!='\0')
     {
-        putchar (toupper(string[i]));
+        putchar (tolower(string[i]));
         i++;
     }
 }
+
+void Inverse(int i, char string[80]){
+    while(string[i]!='\0')
+    {
+        if(string[i] >= 'A' && string[i] <= 'Z') {
+            putchar (tolower(string[i]));
+        }
+        else {
+            putchar (toupper(string[i]));
+        }
+        i++;
+    }
+}
+
 int main()
 {
     printf("Enter Sentence Here:  ");
     scanf("%s", &string);
-    Upper(string);
-    Lower(string);
+    printf("Uppercase:  ");
+    Upper(0,string);
+    printf("\nLowercase:  ");
+    Lower(0,string);
+    printf("\nInverse:  ");
+    Inverse(0,string);
     
     return 0;
 }
