f490b32ee00ac7dbc556019e92946f2a181bd526
Michael Montgomery <michael.m@victorious.com>
2017-06-01 10:03:45 -0700

    initial commit

---
+++ Dice Problem/Dice Problem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Dice Problem
+//
+//  Created by Michael Montgomery on 6/1/17.
+//  Copyright © 2017 Michael Montgomery. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
