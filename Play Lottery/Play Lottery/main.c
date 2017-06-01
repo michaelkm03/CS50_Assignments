1d5f55f5241274f8d5217551fb25544a3388f4c8
Michael Montgomery <michael.m@victorious.com>
2017-05-31 20:53:33 -0700

    update main declaration, loop for generating digits from user input

--- Play Lottery/Play Lottery/main.c
+++ Play Lottery/Play Lottery/main.c
@@ -7,9 +7,41 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int lotteryNumber = 65;
+int digits[2];
+int userInput, first;
+
+
+int main() {
+    
+    printf("Enter your number:  ");
+    scanf("%d", &userInput);
+    
+    first = userInput;
+
+    while(first >= 10)
+    {
+        first = first / 10;
+    }
+    digits[0] = first;
+    digits[1] = userInput % 10;
+//    printf("Digit 1:  %d", first);
+//    printf("Digit 2:  %d", digits[1]);
+
+    
+    if (lotteryNumber == userInput){
+        printf("YOU WIN $100,000!!!\n\n");
+    }
+    
+//    else if (lotteryNumber[i] == 6){
+//        printf("one digit");
+//    }
+//    
+    else {
+        printf("YOU LOSE!\n");
+    }
+    
     return 0;
 }
