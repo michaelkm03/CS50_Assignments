e7e888ab971f256c740f0ce3c9ab4182a9ac9c65
Michael Montgomery <michael.m@victorious.com>
2017-06-01 16:25:46 -0700

    update temp array

--- Arrary Shift/Arrary Shift/main.c
+++ Arrary Shift/Arrary Shift/main.c
@@ -12,44 +12,51 @@
 #include <string.h>
 #include <ctype.h>
 
+int originalArray[100], newArray[100],temp [100];
+int size, i, shiftAmount;
+char shiftDirection;
+long t = 0;
+
+
 int main() {
-
-//    int array[100], size, i;
-//    
-//    printf("How many elements?  ");
-//    scanf("%d", &size);
-//    
-//    printf("### There are %d elements in this array ### \n\n", size);
-//    
-//    for (i = 0; i < size; i++){
-//        scanf("%d", &array[i]);
-//        printf("%d", array[i]);
-//    }
-
     
-struct KeyValueArray
-{
-    int key, array[100], i, n;
-    char* value;
-    char name;
-
-};
-    printf("Name of this list?  ");
-    scanf("%d", &n);
+    srand((unsigned) time(&t));
+    printf("How many elements?  ");
+    scanf(" %d", &(size));
+    printf("### There are %d elements in this array ### \n\n", size);
     
-//printf("How many elements?  ");
-//scanf("%d", &size);
-//
-//    for (i = 0; i < size; i++){
-//        
-//    }
-//    
-//    kv.key = 1;
-//    kv.value = "foo";
-//
-//        for (i = 0; i < size; i++)
-//        printf("Key:  %d \n",kv.key);
-//        printf("Value:  %s \n\n",kv.value);
-//    return 0;
-//
+    // Add random ints to array
+    for (i = 0; i < size; i++){
+        originalArray[i] = 1 + rand() % 100;
+    }
+    fflush( stdout );
+
+    printf("|==================== Your Array ====================|\n\n");
+    for (int i = 0; i < size; i++){
+        printf(" |%d| ",originalArray[i]);
+    }
+    
+    printf("\n\n| =============== Which Direction? ============= |\n\n");
+    printf("L :  Left\nR :  Right\n\n>>>  ");
+    scanf(" %c", &shiftDirection);
+    printf("| =================== How Many? ==================== |\n\n\>>>  ");
+    scanf("%d", &shiftAmount);
+    
+    // Add amount for shifting
+    printf("TEMP --\n");
+    for (int i = 0; i < shiftAmount; i++){
+        temp[i] = originalArray[i];
+        printf(" |%d| ",temp[i]);
+    }
+    printf("\nARRAY --\n\n");
+    
+    // Construct new Array after shift
+    int j = 0;
+    for (int i = shiftAmount; i > 0; i--){
+        newArray[i] = temp[j];
+        printf(" |%d| ",newArray[i]);
+        j++;
+    }
+    
+    return 0;
 }
